c41fe5b0b1cb1fb5a6e7f71a782932f20fe9d0d3
ethan <ethanfritz.t@gmail.com>
2023-07-03 17:02:06 -0400

    init lua file, adding comments for specific sections

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
--- global variables
+-- GLOBAL VARIABLES
 opt = vim.opt;
 map = vim.keymap;
 
+-- PLUGINS
+
+-- OPTIONS
 -- line numbers
 opt.number = true;
 
@@ -10,7 +13,10 @@ opt.shiftwidth = 4;
 opt.softtabstop = 4;
 opt.expandtab = false;
 
--- mappings
+-- colorscheme
+
+-- MAPPINGS
+-- window navigation
 map.set('n', '<C-j>', '<C-w>j');
 map.set('n', '<C-k>', '<C-w>k');
 map.set('n', '<C-h>', '<C-w>h');
